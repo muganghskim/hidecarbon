7bdafa35fe494972c6220975c2086b873f52b271
STD-A <rhgustmfrh@naver.com>
2024-03-25 15:58:40 +0900

    first commit

---
+++ src/main/java/com/hidecarbon/hidecarbon/HidecarbonApplication.java
@@ -0,0 +1,13 @@
+package com.hidecarbon.hidecarbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HidecarbonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HidecarbonApplication.class, args);
+	}
+
+}
